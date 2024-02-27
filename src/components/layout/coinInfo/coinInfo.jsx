162838c22fff4d1549af3157677787f019b061c5
Stanislav Khurtov <skhurtov.work@yandex.ru>
2024-02-27 11:52:41 +0300

    feat: add files coinInfo

--- src/components/layout/coinInfo/coinInfo.jsx
+++ src/components/layout/coinInfo/coinInfo.jsx
@@ -6,7 +6,7 @@ export const CoinInfo = ({coin, withSymbol}) => {
         <Flex align='center'>
             <img src={coin.icon} alt={coin.name} style={{width: '30px', marginRight: '10px'}}/>
             <Typography.Title level={2} style={{margin: 0}}>
-                {withSymbol && <span>(coin.symbol)</span>} {coin.name}
+                {withSymbol && <span>({coin.symbol})</span>} {coin.name}
             </Typography.Title>
         </Flex>
     );
