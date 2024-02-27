aeb7ad593a6392acd8db99d5951098428f006a6e
Stanislav Khurtov <skhurtov.work@yandex.ru>
2024-02-27 11:51:49 +0300

    feat: add files coinInfo

--- src/components/layout/coinInfo/coinInfo.jsx
+++ src/components/layout/coinInfo/coinInfo.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import {Flex, Typography} from "antd";
 
-export const CoinInfo = ({coin}) => {
+export const CoinInfo = ({coin, withSymbol}) => {
     return (
         <Flex align='center'>
             <img src={coin.icon} alt={coin.name} style={{width: '30px', marginRight: '10px'}}/>
             <Typography.Title level={2} style={{margin: 0}}>
-                {coin.name}
+                {withSymbol && <span>(coin.symbol)</span>} {coin.name}
             </Typography.Title>
         </Flex>
     );
--- src/components/ui/coinInfoModal/coinInfoModal.jsx
+++ src/components/ui/coinInfoModal/coinInfoModal.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import {Divider, Flex, Tag, Typography} from "antd";
+import {CoinInfo} from "../../layout/coinInfo/coinInfo.jsx";
 
 export const CoinInfoModal = ({coin}) => {
     return (
         <>
-            <Flex align='center'>
-                <img src={coin.icon} alt={coin.name} style={{width: '30px', marginRight: '10px'}}/>
-                <Typography.Title level={2} style={{margin: 0}}>
-                    ({coin.symbol}) {coin.name}
-                </Typography.Title>
-            </Flex>
+            <CoinInfo coin={coin} withSymbol/>
             <Divider/>
             <Typography.Paragraph>
                 <Typography.Text strong>1 hour: </Typography.Text>
