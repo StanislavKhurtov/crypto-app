3bcb7e04b354520a93c55180e36c9ed338e10de1
Stanislav Khurtov <skhurtov.work@yandex.ru>
2024-02-27 13:21:35 +0300

    feat: change file

--- src/components/layout/appContent/appContent.jsx
+++ src/components/layout/appContent/appContent.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import {Layout} from "antd";
+import {Layout, Typography} from "antd";
+import {useCrypto} from "../../../hooks/useCrypto";
+import {PortfolioChart} from "../../ui/portfolioChart/index.js";
+import {AssetsTable} from "../../ui/assetsTable/index.js";
+
 
 const contentStyle = {
     textAlign: 'center',
@@ -10,10 +14,26 @@ const contentStyle = {
 };
 
 export const AppContent = () => {
+    const { assets, crypto } = useCrypto()
+
+    const cryptoPriceMap = crypto.reduce((acc, c) => {
+        acc[c.id] = c.price
+        return acc
+    }, {})
 
     return (
-        <Layout.Content style={contentStyle}>Content</Layout.Content>
-
+        <Layout.Content style={contentStyle}>
+            <Typography.Title level={3} style={{ textAlign: 'left', color: '#fff' }}>
+                Portfolio:{' '}
+                {assets
+                    .map((asset) => asset.amount * cryptoPriceMap[asset.id])
+                    .reduce((acc, v) => (acc += v), 0)
+                    .toFixed(2)}
+                $
+            </Typography.Title>
+            <PortfolioChart />
+            <AssetsTable />
+        </Layout.Content>
     );
 };
 
